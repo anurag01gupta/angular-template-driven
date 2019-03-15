f4418605b2a3b26f6aea1e2d015b69747245ea10
Anurag Gupta <email@anuraggupta.in>
2019-03-15 17:03:23 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template-driven', component: TemplateDrivenComponent
+  },
+  {
+    path: '', redirectTo: 'template-driven', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
